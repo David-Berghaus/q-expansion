82e890ba5a56d479cf31bb93d0c7df9041f490d2
David Berghaus <s6ddberg@uni-bonn.de>
2021-03-15 15:35:26 +0100

    Added get_coefficients_arb_wrap

--- point_matching_arb_wrap.pyx
+++ point_matching_arb_wrap.pyx
@@ -1,4 +1,5 @@
 import math
+from cysignals.signals cimport sig_on, sig_off
 
 from sage.libs.arb.arb cimport *
 from sage.libs.arb.acb cimport *
@@ -68,7 +69,9 @@ cdef _compute_V_block_matrix_arb_wrap(acb_mat_t V_view,acb_mat_t J,int Ms,int Mf
     cdef acb_mat_t W
     acb_mat_init(W, coord_len, Mf-Ms+1)
     _get_W_block_matrix_arb_wrap(W,Ms,Mf,weight,coordinates,bit_prec)
+    sig_on()
     acb_mat_approx_mul(V_view,J,W,bit_prec)
+    sig_off()
     acb_mat_clear(W) #It would be more efficient to re-use these allocations...
 
 cdef _compute_V_block_matrix_normalized_column_arb_wrap(acb_mat_t b_view,acb_mat_t J,int cii,int cjj,int l_normalized,int weight,Y,coordinates,int bit_prec): #Computes column of V corresponding to l_normalized
@@ -76,7 +79,9 @@ cdef _compute_V_block_matrix_normalized_column_arb_wrap(acb_mat_t b_view,acb_mat
     cdef acb_mat_t W
     acb_mat_init(W, coord_len, 1)
     _get_W_block_matrix_arb_wrap(W,l_normalized,l_normalized,weight,coordinates,bit_prec)
+    sig_on()
     acb_mat_approx_mul(b_view,J,W,bit_prec)
+    sig_off()
     acb_mat_clear(W) #It would be more efficient to re-use these allocations...
 
 cdef _subtract_diagonal_terms(acb_mat_t V_view,int Ms,int Mf,int weight,Y,int bit_prec): #transforms V to V_tilde by subtracting the diagonal elements
@@ -91,21 +96,6 @@ cdef _subtract_diagonal_terms(acb_mat_t V_view,int Ms,int Mf,int weight,Y,int bi
     for i in range(Ms,Mf+1):
         tmp = Y_pow_weight_half*((-two_pi*i*Y).exp())
         acb_sub_arb(acb_mat_entry(V_view,i-Ms,i-Ms),acb_mat_entry(V_view,i-Ms,i-Ms),tmp.value,bit_prec)
-
-# cdef _compute_V_tilde_block_matrix_arb_wrap(acb_mat_t V_view,acb_mat_t J,int cii,int cjj,int Ms,int Mf,int weight,Y,coordinates,int bit_prec): #computes a V_tilde-block-matrix and stores it in V
-#     _compute_V_block_matrix_arb_wrap(V_view,J,Ms,Mf,weight,coordinates,bit_prec)
-#     cdef int M = Mf-Ms+1
-#     cdef int weight_half, i
-#     cdef RR = RealBallField(bit_prec)
-#     cdef CC = ComplexBallField(bit_prec)
-#     cdef RealBall Y_pow_weight_half, two_pi, tmp
-#     if cii == cjj:
-#         weight_half = weight//2
-#         Y_pow_weight_half = Y**weight_half
-#         two_pi = 2*get_pi_ball(bit_prec)
-#         for i in range(Ms,M+1):
-#             tmp = Y_pow_weight_half*((-two_pi*i*Y).exp())
-#             acb_sub_arb(acb_mat_entry(V_view,i-Ms,i-Ms),acb_mat_entry(V_view,i-Ms,i-Ms),tmp.value,bit_prec)
 
 cpdef get_V_matrix_arb_wrap(S,int M,Y,int weight,int bit_prec):
     cdef int Ms = 1
@@ -196,12 +186,12 @@ cpdef get_V_tilde_matrix_b_arb_wrap(S,int M,Y,int weight,int multiplicity,int bi
             coordinates = pb[cii][cjj]
             coord_len = len(coordinates)
             acb_mat_window_init(V_view,V.value,cii*M,cjj*M,(cii+1)*M,(cjj+1)*M)
+            Msjj = len(normalization[cjj])+1
+            Mfjj = Msjj+M-1
+            Msii = len(normalization[cii])+1
+            Mfii = Msii+M-1
             if coord_len != 0:
-                acb_mat_init(J, M, coord_len)
-                Msjj = len(normalization[cjj])+1
-                Mfjj = Msjj+M-1
-                Msii = len(normalization[cii])+1
-                Mfii = Msii+M-1
+                acb_mat_init(J,M,coord_len)
                 _get_J_block_matrix_arb_wrap(J,Msii,Mfii,weight,Q,coordinates,bit_prec) #we compute J here to re-use it later for normalized column
                 _compute_V_block_matrix_arb_wrap(V_view,J,Msjj,Mfjj,weight,coordinates,bit_prec)
                 if cjj == 0:
@@ -209,21 +199,48 @@ cpdef get_V_tilde_matrix_b_arb_wrap(S,int M,Y,int weight,int multiplicity,int bi
                     l_normalized = _get_l_normalized(cjj,normalization)
                     _compute_V_block_matrix_normalized_column_arb_wrap(b_view,J,cii,cjj,l_normalized,weight,Y,coordinates,bit_prec)
                     acb_mat_window_clear(b_view)
+                acb_mat_clear(J)
             if cii == cjj:
                 _subtract_diagonal_terms(V_view,Msjj,Mfjj,weight,Y,bit_prec)
             acb_mat_window_clear(V_view)
+    sig_on()
     acb_mat_neg(b.value, b.value)
+    sig_off()
     return V,b
 
-# def get_coefficients_arb_wrap(S,int weight,int multiplicity,Y=0,int M=0,digit_prec=14):
-#     bit_prec = digits_to_bits(digit_prec)
+def get_coefficients_arb_wrap(S,int weight,int multiplicity,int digit_prec,Y=0,int M=0):
+    bit_prec = digits_to_bits(digit_prec)
+    RR = RealBallField(bit_prec)
+    if float(Y) == 0: #This comparison does not seem to be defined for arb-types...
+        Y = RR(S.group().minimal_height()*0.8)
+    if M == 0:
+        M = math.ceil(get_M_for_holom(Y,weight,digit_prec))
+    print("Y = ", Y)
+    print("M = ", M)
+    cdef Matrix_complex_ball_dense V,b
+    V,b = get_V_tilde_matrix_b_arb_wrap(S,M,Y,weight,multiplicity,bit_prec)
+    sig_on()
+    acb_mat_approx_solve(b.value,V.value,b.value,bit_prec)
+    sig_off()
+    return b[:M]
+
+# def test_low_precision_inverse(S): #Test if matrix can be inverted with much lower precision so that we can later use it as a preconditioner
+#     bit_prec = digits_to_bits(500)
 #     RR = RealBallField(bit_prec)
-#     if float(Y) == 0: #This comparison does not seem to be defined for arb-types...
-#         Y = RR(S.group().minimal_height()*0.8)
-#     if M == 0:
-#         M = math.ceil(1.2*get_M_for_holom(Y,weight,prec))
-#     print("Y = ", Y)
-#     print("M = ", M)
-#     V,b = get_V_tilde_matrix_b_arb_wrap(S,M,Y,weight,multiplicity,bit_prec)
-#     acb_mat_approx_solve(b.value,V.value,b.value,bit_prec)
-#     return b
+#     Y = RR(S.group().minimal_height()*0.8)
+#     M = 376
+#     cdef Matrix_complex_ball_dense V,b
+#     V,b = get_V_tilde_matrix_b_arb_wrap(S,M,Y,12,1,bit_prec)
+#     print "V computed"
+#     cdef acb_mat_t inv_low, inv_high
+#     acb_mat_init(inv_low, M, M)
+#     acb_mat_init(inv_high, M, M)
+#     acb_mat_approx_inv(inv_low,V.value,64)
+#     print "low inverse computed"
+#     acb_mat_approx_inv(inv_high,V.value,bit_prec)
+#     print "high inverse computed"
+#     acb_printd(acb_mat_entry(inv_low,0,0), 16)
+#     acb_printd(acb_mat_entry(inv_high,0,0), 16)
+#     print('')
+#     acb_printd(acb_mat_entry(inv_low,0,M-1), 16)
+#     acb_printd(acb_mat_entry(inv_high,0,M-1), 16)
