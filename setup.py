b4c1c025cba89e875f2aac82a8d5b56e79caf90f
David Berghaus <s6ddberg@uni-bonn.de>
2021-03-20 13:56:42 +0100

    Added iterative solver for 64 bit and some acb_mat_approx routines

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ import Cython.Compiler.Options
 Cython.Compiler.Options.annotate=True
 
 setup(
-    ext_modules=cythonize(["my_pullback.pyx","point_matching_dp.pyx","point_matching_arb_wrap.pyx"],annotate=True),
+    ext_modules=cythonize(["my_pullback.pyx","point_matching_dp.pyx","point_matching_arb_wrap.pyx","iterative_solvers.pyx"],annotate=True),
     zip_safe=False,
 )
