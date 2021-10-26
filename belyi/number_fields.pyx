ad1de0034e22b3747f78310ea8f187bc54fc28fa
David Berghaus <s6ddberg@uni-bonn.de>
2021-10-26 18:11:32 +0200

    Added functionality to work with reduced numberfields

--- belyi/number_fields.pyx
+++ belyi/number_fields.pyx
@@ -4,6 +4,10 @@ from sage.rings.real_mpfr import RealField
 from sage.rings.qqbar import QQbar
 # from sage.rings.complex_mpfr import ComplexField #This only works in Sage/9.3
 from sage.interfaces.gp import gp
+from sage.interfaces.gp import pari
+from sage.symbolic.constants import pi
+from sage.rings.integer_ring import ZZ
+from sage.rings.polynomial.polynomial_ring_constructor import PolynomialRing
 
 from point_matching.point_matching_arb_wrap import digits_to_bits
 
@@ -18,34 +22,97 @@ cpdef get_decimal_digit_prec(epsilon):
         eps_approx = RF(epsilon).abs() #There is no reason to perform this computation at full precision
         return -int(eps_approx.log10())
 
-cpdef to_QQbar(x, max_extension_field_degree, check_if_poly_has_max_degree=False):
-    """
-    Tries to express x as an element of QQbar (this expression might not be correct).
-    If check_if_poly_has_max_degree == True then also return if the polynomial is of maximal order which
-    makes it likely that the expression is not a true solution.
-    """
-    numberfield = x.algebraic_dependency(max_extension_field_degree)
-    roots = numberfield.roots(ring=QQbar,multiplicities=False)
+# cpdef to_QQbar(x, max_extension_field_degree, check_if_poly_has_max_degree=False):
+#     """
+#     Tries to express x as an element of QQbar (this expression might not be correct).
+#     If check_if_poly_has_max_degree == True then also return if the polynomial is of maximal order which
+#     makes it likely that the expression is not a true solution.
+#     """
+#     numberfield = x.algebraic_dependency(max_extension_field_degree)
+#     roots = numberfield.roots(ring=QQbar,multiplicities=False)
 
-    #Now we need to recognize to which root our expression corresponds. Is there a better way for this?
-    diffs = [(root-x).abs() for root in roots]
-    root_index = diffs.index(min(diffs))
+#     #Now we need to recognize to which root our expression corresponds. Is there a better way for this?
+#     diffs = [(root-x).abs() for root in roots]
+#     root_index = diffs.index(min(diffs))
 
-    if check_if_poly_has_max_degree == False:
-        return roots[root_index]
+#     if check_if_poly_has_max_degree == False:
+#         return roots[root_index]
+#     else:
+#         return roots[root_index], numberfield.degree()==max_extension_field_degree
+
+cpdef to_QQbar(x, gen, extension_field_degree, check_if_result_is_invalid=True):
+    """
+    Try to express x as an algebraic number in terms of gen.
+
+    If check_if_result_is_invalid == True then also place pi into LLL-basis (therefore, if the coefficient of pi is non-zero
+    then the detected solution is not valid) and if result is invalid, return False.
+    """
+    gen_approx = x.parent(gen)
+    LLL_basis = [x]
+    for i in range(extension_field_degree):
+        LLL_basis.append(gen_approx**i)
+    if check_if_result_is_invalid == True:
+        LLL_basis.append(x.parent(pi))
+    LLL_res = lindep(LLL_basis)
+    if check_if_result_is_invalid == True and LLL_res[-1] != 0: #Result cannot be correct because pi is not part of true basis
+        return False
+    
+    x_alg = 0
+    for i in range(extension_field_degree):
+        x_alg += LLL_res[i+1]*gen**i
+    x_alg /= -LLL_res[0]
+    return x_alg   
+
+def get_numberfield_and_gen(x, max_extension_field_degree, check_if_result_is_invalid=True, reduce_numberfield=True):
+    """
+    Try to express x in a numberfield over QQbar with specified max_extension_field_degree.
+    This function returns a polynomial over which the numberfield is defined as well as the generator (expressed as an algebraic number).
+
+    If check_if_result_is_invalid == True then also place pi into LLL-basis (therefore, if the coefficient of pi is non-zero
+    then the detected solution is not valid) and if result is invalid, return False.
+
+    If reduce_numberfield == True then try to reduce the numberfield and return the generator of this numberfield.
+    It is advised to only use this option if one is certain that the result is valid, because this function might take very long for
+    spurious numberfields.
+    """
+    LLL_basis = [x**i for i in range(max_extension_field_degree+1)]
+    if check_if_result_is_invalid == True:
+        LLL_basis.append(x.parent(pi))
+    LLL_res = lindep(LLL_basis)
+    if check_if_result_is_invalid == True and LLL_res[-1] != 0: #Result cannot be correct because pi is not part of true basis
+        return False
+    
+    P = PolynomialRing(ZZ,"x")
+    numberfield = P(LLL_res)
+    if reduce_numberfield == False:
+        #Now we need to recognize to which root our expression corresponds. Is there a better way for this?
+        roots = numberfield.roots(ring=QQbar,multiplicities=False)
+        diffs = [(root-x).abs() for root in roots]
+        root_index = diffs.index(min(diffs))
+        gen = roots[root_index]
+        return numberfield, gen
     else:
-        return roots[root_index], numberfield.degree()==max_extension_field_degree
+        numberfield_red = P(pari.polredabs(numberfield))
+        if numberfield_red.degree() == 1:
+            return numberfield_red, 1
+        else:
+            #Now we need to recognize to which root our expression corresponds. Is there a better way for this?
+            roots = numberfield_red.roots(ring=QQbar,multiplicities=False)
+            for root in roots:
+                root_approx = x.parent(root)
+                LLL_basis = [x]
+                for i in range(numberfield_red.degree()):
+                    LLL_basis.append(root_approx**i)
+                LLL_basis.append(x.parent(pi))
+                LLL_res = lindep(LLL_basis)
+                if LLL_res[-1] == 0: #This seems to be the correct generator
+                    return numberfield_red, root
+            raise ArithmeticError("We should not get here!")
 
-cpdef gp_lindep(x, int correct_digits, int max_order): #Not sure if this function will be useful
-    bit_prec = digits_to_bits(correct_digits)
-    CF = MPComplexField(bit_prec)
-    x_cf = CF(x.real(),x.imag())
-    x_str = x_cf.str()
-    gp("x = " + x_str)
-    gp_command = "lindep([1"
-    for i in range(1,max_order+1):
-        gp_command += ",x^" + str(i)
-    gp_command += "])"
-    lindep_res = gp(gp_command).sage()
-    print("Warning, we have not set default(realprecision) correctly!")
-    return lindep_res
+def lindep(L):
+    """
+    Given a list L, use PARI to return a list of factors F_i s.t. sum_i F_i*L_i = 0.
+    """
+    F = pari(L).lindep().list()
+    res = [f.sage() for f in F]
+    return res
