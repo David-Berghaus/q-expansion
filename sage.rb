d0a9c3199c830ffb02835b4d55f743508ae2500b
David Berghaus <s6ddberg@uni-bonn.de>
2023-03-02 10:14:23 +0100

    Updated SHA

--- sage.rb
+++ sage.rb
@@ -5,7 +5,7 @@ cask "sage" do
 
   version "9.2"
   sha256 arm:   "faae6f5c251c03e66ad70c58e61ae2f3d5f3894a133bd0b2b04f05ad230b8c91",
-         intel: "70f315b7a47d95b45fe8902838fbe6dd3cfdfe4eb761dba2df644092d640cf68"
+         intel: "34854b90e8521df102a5d59ed5be42a58ecafc0cac18cd0e257c4051a015dbf5"
 
   url "https://github.com/3-manifolds/Sage_macOS/releases/tag/v1.0",
       verified: "github.com/3-manifolds/Sage_macOS/"
